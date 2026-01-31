63ca514c3baa631505fb181e24b7c05e8c46b3c2
PhoenixAlpha204 <68132454+PhoenixAlpha204@users.noreply.github.com>
2026-01-30 16:59:54 -0800

    feat: list formatting for history page

--- app/src/main/java/com/phoenixalpha/dicey/ui/screens/History.kt
+++ app/src/main/java/com/phoenixalpha/dicey/ui/screens/History.kt
@@ -2,11 +2,18 @@ package com.phoenixalpha.dicey.ui.screens
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,16 +28,60 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.phoenixalpha.dicey.DiceViewModel
+import com.phoenixalpha.dicey.data.Result
 
 @Composable
 fun History(viewModel: DiceViewModel = hiltViewModel()) {
-    val results = viewModel.results.collectAsState()
+    fun calculateShape(index: Int, lastIndex: Int): RoundedCornerShape {
+        val outerRadius = 15.dp
+        val innerRadius = 5.dp
 
-    LazyColumn(reverseLayout = true) {
-        item { Spacer(Modifier.padding(bottom = 60.dp)) }
-        items(results.value) {
-            Text(it.toString(), Modifier.padding(vertical = 20.dp))
+        return when (index) {
+            lastIndex -> RoundedCornerShape(
+                outerRadius,
+                outerRadius,
+                innerRadius,
+                innerRadius
+            )
+
+            0 -> RoundedCornerShape(
+                innerRadius,
+                innerRadius,
+                outerRadius,
+                outerRadius
+            )
+
+            else -> RoundedCornerShape(innerRadius)
         }
+    }
+
+    val results = viewModel.results.collectAsState().value
+
+    LazyColumn(
+        contentPadding = PaddingValues(12.dp),
+        reverseLayout = true,
+        verticalArrangement = Arrangement.spacedBy(3.dp)
+    ) {
+        item { Spacer(Modifier.padding(bottom = 65.dp)) }
+        itemsIndexed(results) { index, item ->
+            val lastIndex = results.lastIndex
+            val shape = calculateShape(index, lastIndex)
+
+            HistoryItem(shape, item)
+        }
+    }
+}
+
+@Composable
+private fun HistoryItem(shape: RoundedCornerShape, item: Result) {
+    Surface(shape = shape) {
+        ListItem(
+            { Text(item.result) },
+            supportingContent = { Text(item.diceRolled) },
+            colors = ListItemDefaults.colors(
+                MaterialTheme.colorScheme.surfaceContainerHigh
+            )
+        )
     }
 }
 
