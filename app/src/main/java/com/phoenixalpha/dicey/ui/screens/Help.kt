784efbbba15a7169ad8cc9799cb6b3631f18c297
PhoenixAlpha204 <68132454+PhoenixAlpha204@users.noreply.github.com>
2026-02-23 00:11:31 -0800

    feat: app info section in Help

--- app/src/main/java/com/phoenixalpha/dicey/ui/screens/Help.kt
+++ app/src/main/java/com/phoenixalpha/dicey/ui/screens/Help.kt
@@ -2,14 +2,18 @@ package com.phoenixalpha.dicey.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -37,13 +41,35 @@ fun Help() {
     )
 
     LazyColumn(
-        Modifier.padding(start = 16.dp, end = 16.dp, top = 12.dp)
+        Modifier.padding(12.dp, 12.dp, 12.dp)
     ) {
+        item { About() }
         helpEntries.forEach { (header, content) ->
             stickyHeader { Header(header) }
             item { Content(content) }
         }
         item { Spacer(Modifier.padding(bottom = 12.dp)) }
+    }
+}
+
+@Composable
+private fun About() {
+    val uriHandler = LocalUriHandler.current
+    val repoUrl = "https://github.com/PhoenixAlpha204/Dicey"
+
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(vertical = 30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Dicey",
+            Modifier.padding(vertical = 20.dp),
+            style = typography.displayLarge
+        )
+        Button({ uriHandler.openUri(repoUrl) }) { Text("Source Code") }
+        Text("ⓒ PhoenixAlpha", fontSize = 12.sp)
     }
 }
 
